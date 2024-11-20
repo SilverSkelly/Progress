ca52980308d91e072702fd4bb0a085e6deec7ec8
taswakil <designerfairytasneem@gmail.com>
2024-11-20 11:40:43 -0600

    create fundamentals vocab page

---
+++ frontend/src/pages/Fundamentals.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter, Link } from 'react-router-dom';
+import Syntax from '../vocabcards/Fundamentals_vocab.jsx/syntax';
+
+function Fundamentals() {
+  return (
+<div>
+    
+    <div><h1>Fundamentals Vocab</h1></div>
+    <Syntax/>
+    </div>
+  )
+}
+
+export default Fundamentals
