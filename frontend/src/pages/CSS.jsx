3548a0f0ebfa7a074d26c8745526db90b868843d
taswakil <designerfairytasneem@gmail.com>
2024-11-20 16:06:30 -0600

    trying if css works

--- frontend/src/pages/CSS.jsx
+++ frontend/src/pages/CSS.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Link } from 'react-router-dom';
+import Word from '../vocabulary_cards/CSS_vocab.jsx/vocabword';
 
 
 function CSS() {
   return (
 <div>
     <div><h1>CSS Vocab</h1></div>
+    <Word/>
     </div>
   )
 }
---
+++ frontend/src/vocabulary_cards/CSS_vocab.jsx/vocabword.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter, Link } from 'react-router-dom';
+
+/// this is a vocab card component for Fundamentals page. You can copy and paste this code for other vocab cards ///
+
+function Word() {
+  return (
+    <div className="vocabCard">
+        <h2>Word</h2>
+    </div>
+    
+  )
+}
+ 
+export default Word
